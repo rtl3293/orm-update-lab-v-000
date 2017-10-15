37ffdf1d505caf498138560309ff4188d1a84091
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-15 03:54:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,8 @@ class Student
   def self.new_from_db(row)
     self.new(row[0], row[1], row[2])
   end
-  def find_by_name(name)
-    Stud
+  def self.find_by_name(name)
+
   end
 
   def self.create(name, grade)
