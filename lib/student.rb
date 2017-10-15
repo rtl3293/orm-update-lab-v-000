22ea8259d1da1dcf5129335c29574575c1d47fa1
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-15 03:25:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,5 +6,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade)
-
+    @name = name
+    @grade = grade
+  end
+  
 end
